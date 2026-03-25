f43c7b08c544689cf30b21fdcd4c7197dee4ded0
Pranali95429 <pranali.ture@gladowl.com>
2026-03-25 16:09:02 +0530

    push1

--- GBSRC/testimonials.php
+++ GBSRC/testimonials.php
@@ -1,6 +1,6 @@
 <!-- Testimonials----------------------------------------------------------------------------------------------------------- -->
         <section class="bg-grey pb-5" id="testimonals">
-            <div class="container rounded-3 px-lg-5 px-0 py-5 ">
+            <div class="container rounded-3 px-lg-1 px-0 py-1 ">
                 <div class="row justify-content-end align-items-center">
                     <div class="col-lg-12 text-center">
                         <h2 class="text-red fw-normal mb-2 display-5 title text-center">Testimonials</h2>
@@ -16,12 +16,14 @@
 
                                 <div
                                     class="d-flex flex-column flex-lg-row align-items-center align-items-lg-start gapStudentName w-full pt-0 pt-lg-3">
-                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/1.jpg" alt="">
+                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/1.png" alt="">
 
                                     <div class="d-flex ps-lg-3 ps-2 flex-column justify-content-center">
                                         <p class="text-justify testimonial-text">My journey at Global Business School and Research Centre has been nothing short of transformative. As an Agri Business Management student, I had the opportunity to connect theory with real-world insights through industrial visits, certification programs, and expert guest sessions. Serving as the General Secretary of the Student Council further sharpened my leadership and organizational skills. From celebrating vibrant festivals on campus to participating in skill-building workshops, every moment here was a learning experience. The faculty support, personalized placement training, and the career guidance I received played a pivotal role in my placement at HDFC Bank. I feel proud and prepared to step into the industry, carrying with me the values and vision of GBSRC.</p>
 
                                         <p class="studentName_01 text-dark fw-bold mb-0">Aaditya Katkar </p>
+                                        <p class="studentName_01 text-dark  mb-0">MBA (Agri Business Management) | Batch 2023–25 </p>
+                                        <p class="studentName_01 text-dark  mb-0"><b>Placed at:</b> HDFC Bank as Agriculture Officer </p>
                                     </div>
                                 </div>
 
@@ -38,12 +40,14 @@
 
                                 <div
                                     class="d-flex flex-column flex-lg-row align-items-center align-items-lg-start gapStudentName w-full pt-0 pt-lg-3">
-                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/2.jpg" alt="">
+                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/2.png" alt="">
 
                                     <div class="d-flex ps-lg-3 ps-2 flex-column justify-content-center">
                                         <p class="text-justify testimonial-text">GBSRC has been a turning point in my academic and professional journey. As an Agri Business Management student, I was exposed to a perfect blend of practical learning and industry exposure. The curriculum is dynamic, and the constant encouragement from faculty pushed me to explore beyond textbooks. My placement at AMUL is a reflection of the continuous support and structured guidance provided by the placement cell. The certification courses, hands-on training, and real-world case studies helped me gain the confidence to step into the corporate world. I’ll always cherish the vibrant culture of GBSRC—be it festival celebrations, industrial visits, or teamwork during college events. These experiences have not only made me industry-ready but also personally enriched.</p>
 
                                         <p class="studentName_01 text-dark fw-bold mb-0">Bhakti Kumbhar</p>
+                                        <p class="studentName_01 text-dark  mb-0">MBA (Agri Business Management) | Batch 2023–25 </p>
+                                        <p class="studentName_01 text-dark  mb-0"><b>Placed at:</b> AMUL as Territory Sales In-charge - I </p>
                                     </div>
                                 </div>
 
@@ -60,12 +64,14 @@
 
                                 <div
                                     class="d-flex flex-column flex-lg-row align-items-center align-items-lg-start gapStudentName w-full pt-0 pt-lg-3">
-                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/3.jpg" alt="">
+                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/3.png" alt="">
 
                                     <div class="d-flex ps-lg-3 ps-2 flex-column justify-content-center">
                                         <p class="text-justify testimonial-text">Choosing GBSRC for my MBA in Agri Business Management has been one of the best decisions of my life. The institute’s focus on holistic development helped me grow academically and professionally. With activities like industry interactions, field visits, certification programs, and practical projects, I gained valuable insights into the real-world application of agri-business concepts. The placement team at GBSRC guided me throughout the recruitment process, and their personalized support helped me secure a position at HDFC Life Insurance. I’ll always remember the vibrant student life here—cultural events, teamwork during festivals, and the friendships I’ve made. GBSRC has not only shaped my career but also given me memories for a lifetime</p>
 
                                         <p class="studentName_01 text-dark fw-bold mb-0">Omkar Chavan </p>
+                                        <p class="studentName_01 text-dark  mb-0">MBA (Agri Business Management) | Batch 2023–25 </p>
+                                        <p class="studentName_01 text-dark  mb-0"><b>Placed at:</b> HDFC Life Insurance as Executive Trainee </p>
                                     </div>
                                 </div>
 
@@ -82,12 +88,14 @@
 
                                 <div
                                     class="d-flex flex-column flex-lg-row align-items-center align-items-lg-start gapStudentName w-full pt-0 pt-lg-3">
-                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/4.jpg" alt="">
+                                    <img class="studentImg mb-2 mb-lg-0 " src="images/testimonal/4.png" alt="">
 
                                     <div class="d-flex ps-lg-3 ps-2 flex-column justify-content-center">
                                         <p class="text-justify testimonial-text">My experience at Global Business School and Research Centre has been truly enriching. Pursuing a dual specialization in Finance and Marketing allowed me to explore diverse dimensions of management with practical exposure and academic excellence. GBSRC’s ecosystem is built to nurture talent through industry-oriented certification courses, interactive guest lectures, and hands-on learning. The institute’s personalized approach to placement training played a key role in helping me secure a position at Yotta as a Senior Executive. Beyond academics, what makes GBSRC special is its vibrant campus life—celebrating festivals, engaging in student-driven initiatives, and being part of a culture that fosters leadership and innovation. I’m proud to be a part of this transformative journey</p>
 
                                         <p class="studentName_01 text-dark fw-bold mb-0">Purusharthwardhan Rathore </p>
+                                        <p class="studentName_01 text-dark  mb-0">MBA (Finance and Marketing) | Batch 2023–25</p>
+                                        <p class="studentName_01 text-dark  mb-0"><b>Placed at:</b> Yotta as Senior Executive</p>
                                     </div>
                                 </div>
 
