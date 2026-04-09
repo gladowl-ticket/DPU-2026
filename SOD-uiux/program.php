eca3967de08f0563d8e15661a4b7e51b9977796a
Pranali95429 <pranali.ture@gladowl.com>
2026-04-09 17:46:04 +0530

    push pranali

---
+++ SOD-uiux/program.php
@@ -0,0 +1,84 @@
+
+
+  <!--====== programs section ==========-->
+  <section class="design-section py-5">
+    <div class="container">
+
+      <!-- Heading -->
+                    <h2 class="mb-lg-3 mb-0 display-5 fw-bold title text-center">Programs <span class="text-red">Offered</span></h2>
+
+      <!-- Top Banner -->
+      <div class="row align-items-center banner-wrapper position-relative">
+        <div class="program-img text-end">
+          <img class="lazyload " src="img/programs/001.png">
+        </div>
+        <!-- Left Content -->
+        <div class="col-md-7">
+          <div class="banner-left">
+            <h5>User Experience Design (B.DES Programme) </h5>
+
+            <div class="row mt-4">
+              <div class="col-12">
+                <p class="text-white fw-bold"><b>Eligibility:</b></p>
+                <p class="">Candidates must be 10 + 2 from any discipline and from a recognized university or board with a minimum of 45% for Open and minimum of 40% for others. Freshers may also apply.</p>
+              </div>
+
+              <div class="col-12">
+                <p class="text-white fw-bold"><b>Duration:</b></p>
+                <p class="">4 years full-time</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Bottom Box -->
+      <div class="program-box mt-5">
+        <div class="row">
+          <!-- B.DES -->
+          <div class="col-md-12 border-left-custom">
+            <h5>Program Highlights</h5>
+            <p>UIUX Design also known as Interaction Design is built for creators who want to craft meaningful digital experiences. As one of India's emerging ui ux design course in Pune destinations, DPU blends creativity, compassion, and technology to prepare students for the fast-paced world of digital products and interfaces.<br>
+            From websites and mobile apps to smart interfaces and immersive ecosystems, students learn to design intuitive digital journeys that align with human behaviour. With modern studios, expert mentorship, and real-world exposure, DPU continues to rise among the best colleges for UI UX design in India.</p>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Top Banner -->
+      <div class="row align-items-center banner-wrapper position-relative pt-5">
+         <div class="program-img text-end">
+          <img class="lazyload " src="img/programs/001.png">
+        </div>
+        <!-- Left Content -->
+        <div class="col-md-7">
+          <div class="banner-left">
+            <h5>User Experience Design (M.DES Programme)</h5>
+
+            <div class="row mt-4">
+              <div class="col-11">
+                <p class="text-white fw-bold"><b>Eligibility:</b></p>
+                <p class="">Candidates must have a Bachelor's degree in Design, Fine Arts, Architecture, Engineering, or related fields with a minimum of 45% marks for Open category and 40% marks for other categories. Freshers may also apply.</p>
+              </div>
+
+              <div class="col-12">
+                <p class="text-white fw-bold"><b>Duration:</b></p>
+                <p class="">2 years full-time </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Bottom Box -->
+      <div class="program-box mt-5">
+        <div class="row">
+          <!-- B.DES -->
+          <div class="col-md-12 border-left-custom">
+            <h5>Program Highlights</h5>
+            <p>The program blends design thinking, user research, prototyping, usability testing, and interface development. Students explore apps, web platforms, wearable devices, and emerging interactive technologies, learning to design experiences that are accessible, functional, and visually refined.<br>
+            Hands-on projects, workshops, and industry-focused assignments help students build both conceptual understanding and practical skill. Emphasis on empathy and innovation equips graduates to anticipate user needs and craft digital solutions that delight and engage.<br>
+            Graduates of the MDes in UI UX Design program are equipped for careers in digital product companies, app and web design studios, interactive media, and emerging tech spaces designing experiences that people remember, rely on, and enjoy.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
